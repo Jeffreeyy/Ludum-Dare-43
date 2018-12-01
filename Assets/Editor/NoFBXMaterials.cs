0235aa046f3c98b800c6a91d149bc73f8d323fd8
Unknown <t_b@live.nl>
2018-12-01 14:21:16 +0100

    models and other shizzle

---
+++ Assets/Editor/NoFBXMaterials.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+ 
+public class NoFBXMaterials : AssetPostprocessor
+{
+   void OnPreprocessModel()
+   {
+     ModelImporter importer = assetImporter as ModelImporter;
+     importer.importMaterials = false;
+   }
+}
