e9bdcbe19ce3522ade8924dbd27bfb6bad92a10c
Jeffrey Laarman <jeffrey_laarman@hotmail.com>
2018-12-01 09:48:38 +0100

    Initial player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float m_MovementSpeed = 5f;
+    private Vector3 m_Direction;
+
+    private void Start()
+    {
+        m_Direction = Vector3.left;
+    }
+
+
+    void Update()
+    {
+        // Toggle direction
+        if (Input.GetKeyDown(KeyCode.Space))
+            m_Direction = m_Direction == Vector3.left ? Vector3.right : Vector3.left;
+
+        transform.position += new Vector3(m_Direction.x * m_MovementSpeed * Time.deltaTime, 0, m_MovementSpeed * Time.deltaTime);
+    }
+}
