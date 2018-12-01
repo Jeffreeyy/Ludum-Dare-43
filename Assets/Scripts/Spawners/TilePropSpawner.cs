1135c1ae07f5b9612f99137067c2389d9c7c2342
Jeffrey Laarman <jeffrey_laarman@hotmail.com>
2018-12-02 00:18:59 +0100

    Updated tile prop spawns

---
+++ Assets/Scripts/Spawners/TilePropSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePropSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> m_PropPrefabs = new List<GameObject>();
+    [SerializeField] private int m_MinSpawnAmount = 4, m_MaxSpawnAmount = 5;
+    [SerializeField] private Vector2 m_TileBounds = new Vector2(5, 5);
+    [SerializeField] private float m_OffsetFromTileBounds = 0.5f;
+    [Range(0.5f, 3f)]
+    [SerializeField]
+    private float m_MinimumSpacingBetweenProps = 1.5f;
+    [SerializeField] private bool m_RandomRotation = false;
+    [SerializeField] private Vector3 m_MinScale = new Vector3(1f, 1.3f, 1f), m_MaxScale = new Vector3(2f, 2.3f, 2f);
+
+    private List<GameObject> m_SpawnedProps = new List<GameObject>();
+
+    public void Spawn()
+    {
+        int amount = Random.Range(m_MinSpawnAmount, m_MaxSpawnAmount + 1);
+        for (int i = 0; i < amount; i++)
+        {
+            int randomIndex = Random.Range(0, m_PropPrefabs.Count);
+            GameObject prop = Instantiate(m_PropPrefabs[randomIndex], transform, false) as GameObject;
+            prop.transform.localPosition = GetRandomSpawnPosition();
+            prop.transform.localScale = new Vector3(Random.Range(m_MinScale.x, m_MaxScale.x), Random.Range(m_MinScale.y, m_MaxScale.y), Random.Range(m_MinScale.z, m_MaxScale.z));
+            if (m_RandomRotation)
+                prop.transform.localRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            m_SpawnedProps.Add(prop);
+        }
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        Vector3 pos = new Vector3(Random.Range(-(m_TileBounds.x - m_OffsetFromTileBounds), m_TileBounds.x - m_OffsetFromTileBounds), 0, Random.Range(-(m_TileBounds.y - m_OffsetFromTileBounds), m_TileBounds.y - m_OffsetFromTileBounds));
+
+        for (int i = 0; i < m_SpawnedProps.Count; i++)
+        {
+            if (Vector3.Distance(pos, m_SpawnedProps[i].transform.position) < m_MinimumSpacingBetweenProps)
+                return GetRandomSpawnPosition();
+        }
+
+        return pos;
+    }
+}
---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private bool m_Trees;
+    [SerializeField] private bool m_Grass;
+    [SerializeField] private bool m_Sticks;
+    [SerializeField] private bool m_Bounding;
+    [Space]
+    [SerializeField] private TilePropSpawner m_TreeSpawner;
+    [SerializeField] private TilePropSpawner m_GrassSpawner;
+    [SerializeField] private TilePropSpawner m_SticksSpawner;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (m_Trees)
+            m_TreeSpawner.Spawn();
+
+        if (m_Grass)
+            m_GrassSpawner.Spawn();
+
+        if (m_Sticks)
+            m_SticksSpawner.Spawn();
+
+        if (m_Bounding)
+            CreateBound();
+    }
+
+    private void CreateBound()
+    {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        if (collider == null)
+            collider = gameObject.AddComponent<BoxCollider>();
+
+        collider.isTrigger = true;
+        collider.center = new Vector3(0, 5, 0);
+        collider.size = new Vector3(10, 10, 10f);
+
+        gameObject.AddComponent<MapBound>();
+    }
+}
