beaad5fa71b866768462b43ca5fc3607480badad
Jeffrey Laarman <jeffrey_laarman@hotmail.com>
2018-12-01 14:44:27 +0100

    Removed Library

---
+++ Assets/Scripts/Colors/ColorCombinations.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ColorCombination
+{
+    public Colors color1;
+    public Colors color2;
+    public Colors output;
+
+    public ColorCombination(Colors color1, Colors color2, Colors output)
+    {
+        this.color1 = color1;
+        this.color2 = color2;
+        this.output = output;
+    }
+}
+
+public class ColorCombinations
+{
+    public static List<ColorCombination> Combinations = new List<ColorCombination>()
+    {
+        new ColorCombination(Colors.Yellow, Colors.Red, Colors.Orange)
+    };
+
+    public static Colors GetCombinedColor(Colors color1, Colors color2)
+    {
+        try
+        {
+            for (int i = 0; i < Combinations.Count; i++)
+            {
+                if (Combinations[i].color1 == color1 && Combinations[i].color2 == color2 || 
+                    Combinations[i].color1 == color2 && Combinations[i].color2 == color1)
+                    return Combinations[i].output;
+            }
+
+            throw new KeyNotFoundException("Combination does not exist");
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+            return Colors.White;
+        }
+    }
+
+    public static ColorCombination GetRandomCombination()
+    {
+        try
+        {
+            if(Combinations.Count > 0)
+                return Combinations[UnityEngine.Random.Range(0, Combinations.Count)];
+            else
+                throw new Exception("Tried to get a random Color Combination but the list is empty. Please add combinations to the ColorCombinations.cs script");
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Colors/ColorLibrary.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public enum Colors
+{
+    Green,
+    Red,
+    Blue,
+    Yellow,
+    Purple,
+    Orange,
+    White
+}
+
+[Serializable]
+public class ColorItem
+{
+    public Colors color;
+    public Material material;
+}
+
+public class ColorLibrary : MonoBehaviour
+{
+    public static ColorLibrary Instance { get; private set; }
+    [SerializeField] private List<ColorItem> m_Colors;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public ColorItem GetRandomColor()
+    {
+        try
+        {
+            if(m_Colors.Count > 0)
+            {
+                int random = UnityEngine.Random.Range(0, m_Colors.Count);
+                return m_Colors[random];
+            }
+            else
+                throw new Exception("Tried to get a random color from the Color Library but it's empty. Please assign values in the inspector");
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+            return null;
+        }
+    }
+
+    public ColorItem GetColorItem(Colors color)
+    {
+        try
+        {
+            ColorItem colorItem = m_Colors.Find(x => x.color == color);
+
+            if (colorItem != null)
+                return colorItem;
+            else
+                throw new KeyNotFoundException("Given color cannot be found in the Color Library. Please make sure it's added in the inspector");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return null;
+        }
+    }
+
+    public List<Colors> GetColors()
+    {
+        try
+        {
+            List<Colors> temp = new List<Colors>();
+
+            for (int i = 0; i < m_Colors.Count; i++)
+                temp.Add(m_Colors[i].color);
+
+            if (temp.Count > 0)
+                return temp;
+            else
+                throw new Exception("Tried to get all the colors from the Color Library but it's empty. Please assign values in the inspector");
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+            return null;
+        }
+    }
+
+    public Material GetMaterial(Colors color)
+    {
+        try
+        {
+            ColorItem colorItem = GetColorItem(color);
+
+            if (colorItem != null)
+                return colorItem.material;
+            else
+                throw new KeyNotFoundException("Given color cannot be found in the Color Library. Please make sure it's added in the inspector");
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+            return null;
+        }
+    }
+}
