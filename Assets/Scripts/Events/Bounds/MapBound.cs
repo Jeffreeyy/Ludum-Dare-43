09e05a44ef3c2e478510cbb139e4a2637ddfc2f7
Unknown <t_b@live.nl>
2018-12-02 20:42:46 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Events/Bounds/MapBound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class MapBound : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (GameEvents.OnMapBoundHit != null) GameEvents.OnMapBoundHit();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (GameEvents.OnMapBoundLeave != null) GameEvents.OnMapBoundLeave();
+        }
+    }
+}
